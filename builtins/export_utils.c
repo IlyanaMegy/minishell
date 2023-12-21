51ba80a37eb6fc0f92e79876e7164fd04bb65910
Ilyana Megy <ilyanamegy7@gmail.com>
2023-12-21 21:33:55 +0100

    :sparkles: good print of err export

--- builtins/export_utils.c
+++ builtins/export_utils.c
@@ -7,7 +7,9 @@
 */
 int	print_export_err_msg(char *arg)
 {
-	ft_printf("err export %s\n", arg);
+	ft_putstr_fd("minishell: export: `", 2);
+	ft_putstr_fd(arg, 2);
+	ft_putstr_fd("': not a valid identifier\n", 2);
 	return (1);
 }
 
