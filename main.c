27e61cb3205ed05a3837d111308da8a8ee51df9d
Ilyana Megy <ilyanamegy7@gmail.com>
2023-12-28 22:24:23 +0100

    :sparkles: cleaned main

--- main.c
+++ main.c
@@ -14,36 +14,10 @@
 
 int	main(int ac, char **av, char **arg_env)
 {
-	// t_envlst envlst;
 	if (ac > 0 && av)
 	{
-		// ft_memset(&envlst, 0, sizeof(t_envlst));
-		// envlst.first = single_env(NULL, 0);
 		get_env(arg_env);
-		ft_env();
-
-
-		ft_printf("\n\n______________________________________\n\n");
-
-		// ft_unset(av);
-		// ft_env();
-		display_export();
-
-		ft_printf("\n\n______________________________________\n\n");
-
-		// ft_unset(av);
-		// ft_env();
-		display_export();
-		// while (1)
-		// {
-		// 	t_minishell.line = readline("minishell$ ");
-		// 	if (!g_minishell.line)
-		// 		break ;
-		// 	if (is_builtin(g_minishell.line))
-		// 		exec_builtin(av);
-		// }
 		clean_env(single_env(NULL, 0));
 	}
-	
 	return (0);
 }
