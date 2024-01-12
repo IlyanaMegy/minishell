f3a5a28535943d8a4bc38afd36ac448f1bd2d6fc
Ilyana Megy <ilyanamegy7@gmail.com>
2024-01-12 18:53:37 +0100

    :rocket: init io_cmd lst

--- srcs/parsing/cmd_lst.c
+++ srcs/parsing/cmd_lst.c
@@ -12,32 +12,39 @@
 
 #include "../../inc/minishell.h"
 
-t_cmd	*lst_new_cmd(void)
+bool    init_io_cmd(t_cmd **node)
 {
-	t_cmd	*node;
+    (*node)->io_list = (t_io_cmd *)malloc(sizeof(t_io_cmd));
+    (*node)->io_list->next = (t_io_cmd *)malloc(sizeof(t_io_cmd));
+    if (!((*node)->io_list) || !((*node)->io_list->next))
+        return (false);
+    (*node)->io_list->path = NULL;
+    (*node)->io_list->type = IO_IN;
+    (*node)->io_list->here_doc = 0;
+    (*node)->io_list->prev = NULL;
+    (*node)->io_list->next->type = IO_OUT;
+    (*node)->io_list->next->path = NULL;
+    (*node)->io_list->next->here_doc = 0;
+    (*node)->io_list->next->prev = (*node)->io_list;
+    (*node)->io_list->next->next = NULL;
+    return (true);
+}
 
-	node = (t_cmd *)malloc(sizeof(t_cmd));
-	if (!(node))
-		return (NULL);
-	node->io_list = (t_io_cmd *)malloc(sizeof(t_io_cmd));
-	node->io_list->next = (t_io_cmd *)malloc(sizeof(t_io_cmd));
-	if (!(node->io_list) || !(node->io_list->next))
-		return (NULL);
-	(node)->cmd = NULL;
-	(node)->args = NULL;
-	(node)->pipe_out = false;
-	(node)->prev = NULL;
-	(node)->next = NULL;
-	node->io_list->path = NULL;
-	node->io_list->type = -1;
-	node->io_list->here_doc = 0;
-	node->io_list->prev = NULL;
-	node->io_list->next->type = -1;
-	node->io_list->next->path = NULL;
-	node->io_list->next->here_doc = 0;
-	node->io_list->next->prev = node->io_list;
-	node->io_list->next->next = NULL;
-	return (node);
+t_cmd    *lst_new_cmd(bool pipe)
+{
+    t_cmd    *node;
+
+    node = (t_cmd *)malloc(sizeof(t_cmd));
+    if (!(node))
+        return (NULL);
+    if (!init_io_cmd(&node))
+        return (NULL);
+    (node)->cmd = NULL;
+    (node)->args = NULL;
+    (node)->pipe_out = pipe;
+    (node)->prev = NULL;
+    (node)->next = NULL;
+    return (node);
 }
 
 void	lst_add_back_cmd(t_cmd **alst, t_cmd *node)
