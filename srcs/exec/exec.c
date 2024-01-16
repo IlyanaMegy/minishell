bd7b8beb5416bcc4022f7842143d07674f7b77b7
Ilyana Megy <ilyanamegy7@gmail.com>
2024-01-16 15:29:35 +0100

    :sparkles: handled simple pipe and redirs ok

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -19,21 +19,24 @@
  * @param  dir: LEFT or RIGHT command
  * @retval None
 */
-static void	exec_pipe_child(t_data *data, int fd[2], t_cmd_direction dir)
+static void	exec_pipe_child(t_data *data, t_cmd *cmd, int fd[2],
+		t_cmd_direction dir)
 {
 	if (dir == LEFT)
 	{
+		ft_printf("cmd left = %s\n\n", cmd->args[0]);
 		close(fd[0]);
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[1]);
 	}
 	else if (dir == RIGHT)
 	{
+		ft_printf("cmd right = %s\n\n", cmd->args[0]);
 		close(fd[1]);
 		dup2(fd[0], STDIN_FILENO);
 		close(fd[0]);
 	}
-	executie(data, true);
+	exec_simple_cmd(data, cmd, true);
 	clean_program(data);
 	exit(single_exit_s(0, GET));
 }
@@ -50,20 +53,21 @@ static int	exec_pipe(t_data *data)
 	int	pid_sec;
 
 	// signint_child = true
-	if (pipe(fd) < 0)
-		return (ft_putstr_fd("__ERROR_PIPE__:\nError pipe.\n", 2), 1);
+	if (pipe(fd) != 0)
+		return (ft_putstr_fd(strerror(3), 2), 3);
+	// return (ft_putstr_fd("__ERROR_PIPE__:\nError pipe.\n", 2), 1);
 	pid_first = fork();
-	if (pid_first < 0)
-		return (ft_putstr_fd("__ERROR_FORK__:\nError fork.\n", 2), 1);
+	if (pid_first == -1)
+		return (ft_putstr_fd("__ERROR_FORK__:\nError fork.\n", 2), 4);
 	if (!pid_first)
-		exec_pipe_child(data, fd, LEFT);
+		exec_pipe_child(data, data->cmd, fd, LEFT);
 	else
 	{
 		pid_sec = fork();
-		if (pid_sec < 0)
-			return (ft_putstr_fd("__ERROR_FORK__:\nError fork.\n", 2), 1);
+		if (pid_sec == -1)
+			return (ft_putstr_fd("__ERROR_FORK__:\nError fork.\n", 2), 4);
 		if (!pid_sec)
-			exec_pipe_child(data, fd, RIGHT);
+			exec_pipe_child(data, data->cmd->next, fd, RIGHT);
 		else
 			return (close_n_wait(fd, pid_first, pid_sec));
 	}
@@ -73,9 +77,10 @@ static int	exec_pipe(t_data *data)
 /**
  * @note   execution of child
  * @param  data: t_data linked list
+ * @param  cmd : current command to execute
  * @retval exit status
 */
-static int	exec_child(t_data *data, int fork_pid)
+static int	exec_child(t_data *data, t_cmd *cmd, int fork_pid)
 {
 	int		status;
 	t_path	path;
@@ -87,14 +92,14 @@ static int	exec_child(t_data *data, int fork_pid)
 		return (ENO_GENERAL);
 	if (!fork_pid)
 	{
-		status = check_redir(data);
+		status = check_redir(cmd);
 		if (status != ENO_SUCCESS)
 			get_out(data, ENO_GENERAL, env);
-		path = get_path(data->cmd->args[0]);
+		path = get_path(cmd->args[0]);
 		if (path.err.no != ENO_SUCCESS)
 			(err_handler(path.err.msg, path.err.cause), get_out(data,
 						single_exit_s(path.err.no, ADD), env));
-		if (execve(path.path, data->cmd->args, env) == -1)
+		if (execve(path.path, cmd->args, env) == -1)
 			return (free(path.path), get_out(data, single_exit_s(0, GET), env),
 				1);
 		free(path.path);
@@ -108,32 +113,33 @@ static int	exec_child(t_data *data, int fork_pid)
  * @note   execution as simple command
  * @param  data: t_data linked list
  * @param  piped: is it piped ?
+ * @param  cmd: current command to execute
  * @retval exit status
 */
-int	exec_simple_cmd(t_data *data, bool piped)
+int	exec_simple_cmd(t_data *data, t_cmd *cmd, bool piped)
 {
 	int	fork_pid;
 
-	if (!data->cmd->args)
+	if (!cmd->args)
 	{
-		single_exit_s(check_redir(data), ADD);
-		return (reset_stds(data, piped), (single_exit_s(0, GET)
-				&& ENO_GENERAL));
+		single_exit_s(check_redir(cmd), ADD);
+		reset_stds(data, piped);
+		return ((single_exit_s(0, GET) && ENO_GENERAL));
 	}
-	else if (is_builtin(data->cmd->args[0]))
+	else if (is_builtin(cmd->args[0]))
 	{
-		single_exit_s(check_redir(data), ADD);
+		single_exit_s(check_redir(cmd), ADD);
 		if (single_exit_s(0, GET) != ENO_SUCCESS)
 			return (reset_stds(data, piped), ENO_GENERAL);
-		single_exit_s(exec_builtin(data), ADD);
+		single_exit_s(exec_builtin(data, cmd), ADD);
 		return (reset_stds(data, piped), single_exit_s(0, GET));
 	}
 	else
 	{
 		fork_pid = fork();
 		if (fork_pid < 0)
-			return (ft_putstr_fd("__ERROR_FORK__:\nError fork.\n", 2), 1);
-		return (exec_child(data, fork_pid));
+			return (ft_putstr_fd("__ERROR_FORK__:\nError fork.\n", 2), 4);
+		return (exec_child(data, cmd, fork_pid));
 	}
 }
 
@@ -145,10 +151,9 @@ int	exec_simple_cmd(t_data *data, bool piped)
 void	executie(t_data *data, bool piped)
 {
 	if (!data->cmd)
-		single_exit_s(1, ADD);
+		single_exit_s(ENO_GENERAL, ADD);
 	if (data->cmd->next)
 		single_exit_s(exec_pipe(data), ADD);
 	else
-		single_exit_s(exec_simple_cmd(data, piped), ADD);
-	single_exit_s(ENO_GENERAL, ADD);
+		single_exit_s(exec_simple_cmd(data, data->cmd, piped), ADD);
 }
