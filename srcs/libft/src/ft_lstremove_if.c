3ade91fbfea0114eb95436ff0926eddfe43f119b
Ilyana Megy <ilyanamegy7@gmail.com>
2024-01-20 16:22:40 +0100

    :art: normed libft

--- srcs/libft/src/ft_lstremove_if.c
+++ srcs/libft/src/ft_lstremove_if.c
@@ -12,20 +12,20 @@
 
 #include "../../../inc/libft.h"
 
-void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
+	t_list	*cur;
+
 	if (begin_list == NULL || *begin_list == NULL)
-		return;
-
-	t_list *cur = *begin_list;
-
+		return ;
+	cur = *begin_list;
 	if (cmp(cur->content, data_ref) == 0)
 	{
 		*begin_list = cur->next;
 		free(cur);
 		ft_list_remove_if(begin_list, data_ref, cmp);
 	}
-	else // if there is a no else, you cant pass the Moulinette, tryed 2023.09.08
+	else
 	{
 		cur = *begin_list;
 		ft_list_remove_if(&cur->next, data_ref, cmp);
